b808de26b8dfbe78b9168589bda30e48c94ebb80
yuvalmizrahi2 <33813996+yuvalmizrahi2@users.noreply.github.com>
2018-04-16 17:19:45 +0300

    Add files via upload

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -15,10 +15,10 @@ CircularInt::CircularInt(const CircularInt& other)
 CircularInt& CircularInt::operator+=(const int other)
 {
     current_range += other;
-    if(current_range > max_range)
-        current_range %= max_range;
-    while(current_range <= min_range - 1)
-        current_range += max_range;
+    while(current_range > max_range)
+        current_range -= (max_range-min_range+1);
+    while(current_range < min_range)
+        current_range += (max_range-min_range+1);
     return *this;
 }
 CircularInt& CircularInt::operator+=(const CircularInt& other)
@@ -50,6 +50,7 @@ CircularInt& CircularInt::operator*=(const CircularInt& other)
 CircularInt& CircularInt::operator%=(const int other)
 {
     current_range %= other;
+    (*this)+=0;
     return *this;
 }
 CircularInt& CircularInt::operator/=(const int other)
@@ -82,11 +83,6 @@ CircularInt CircularInt::operator--(const int other)
 CircularInt CircularInt::operator-()
 {
     CircularInt copy(*this);
-    if(copy.current_range == copy.max_range)
-        copy.current_range = copy.min_range;
-    else if(copy.current_range == copy.min_range)
-        copy.current_range = copy.max_range;
-    else
-        copy.current_range = copy.max_range - copy.current_range;
+    copy.current_range = copy.max_range - (copy.current_range - copy.min_range);
     return copy;
 }
