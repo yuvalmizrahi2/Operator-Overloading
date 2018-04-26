3aaebd8eb9c1273ac69233abc8a9c91ee650cbed
yuval mizrahi <yuvalmiz9@gmail.com>
2018-04-26 14:17:59 +0300

    add operator

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -155,18 +155,18 @@ bool operator!=(const int num , const CircularInt c1)
 {
     return !(c1.current_range == num);
 }
-// bool operator>(const CircularInt c1 , const CircularInt c2)
-// {
-//     return c1.current_range > c2.current_range;
-// }
-// bool operator>(const CircularInt c1 , const int num)
-// {
-//     return c1.current_range > num;
-// }
-// bool operator>(const int num , const CircularInt c1)
-// {
-//     return c1.current_range > num;
-// }
+bool operator>(const CircularInt c1 , const CircularInt c2)
+{
+    return c1.current_range > c2.current_range;
+}
+bool operator>(const CircularInt c1 , const int num)
+{
+    return c1.current_range > num;
+}
+bool operator>(const int num , const CircularInt c1)
+{
+    return num > c1.current_range;
+}
 // bool operator<(const CircularInt c1 , const CircularInt c2)
 // {
 //     return c1.current_range < c2.current_range;
