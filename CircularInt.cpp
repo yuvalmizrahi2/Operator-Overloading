76fe18df25a12367123b8265861a880224c6cc5a
yuval mizrahi <yuvalmiz9@gmail.com>
2018-04-26 15:02:28 +0300

    add operator

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -26,12 +26,15 @@ CircularInt::CircularInt(const CircularInt& other)
 //     this->current_range = c.current_range;
 //     return *this;
 // }
-// const CircularInt operator+(const int num,const CircularInt& c )
-// {
-//     CircularInt copy(c);
-//     copy += num;
-//     return copy;
-// }
+const CircularInt operator+(const int num,const CircularInt& c )
+{
+    int temp(num);
+    CircularInt copy(c);
+    temp += copy.current_range;
+    copy.current_range = temp;
+    copy += 0;
+    return copy;
+}
 const CircularInt operator+(const CircularInt& c , const int num)
 {
     CircularInt copy(c);
